35a2ffe733eef19478a21e2c447b16741e810604
BrysonLong1 <bnlong31@gmail.com>
2025-11-03 19:33:23 -0800

    Initial commit - Trovix CAPI backend setup

---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+﻿from flask import Flask
+from .routes.health import health_bp
+
+def create_app():
+    app = Flask(__name__)
+    # you can load env here later if needed
+    app.register_blueprint(health_bp)
+    return app
---
+++ app/routes/test.py
@@ -0,0 +1,7 @@
+﻿from flask import Blueprint, jsonify
+
+test_bp = Blueprint("test", __name__)
+
+@test_bp.get("/ping")
+def ping():
+    return jsonify({"pong": True}), 200
